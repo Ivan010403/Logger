a584b049b74c63589730fd9990718d12868610f2
Ivan010403 <vany.kozlov5@mail.ru>
2025-01-17 21:20:36 +0500

    shared_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,47 @@
 #include <string>
+#include <fstream>
+#include <memory>
+#include <iostream>
 
 enum class LoggerLevel: unsigned char {INFO, WARN, ERROR};
 
+
+
 class Logger {
 public:
-    Logger() = delete;
+    Logger(const std::string filename, LoggerLevel level) : 
+        filename_(filename), 
+        default_level_(level) 
+    {
+        logger_file_ = std::make_shared<std::ofstream> (filename, std::ios_base::app);
+    }
 
-    Logger(const std::string filename, LoggerLevel level) : filename_(filename), default_level_(level) {}
-
-    Logger(const Logger& src) = default;
-
-    Logger(Logger&& src) = default;
+    Logger(const Logger& src) : 
+        logger_file_(src.logger_file_),
+        filename_(src.filename_),
+        default_level_(src.default_level_) {}
 
     ~Logger() = default;
 
-    // There are no needs for this operator
+    Logger() = delete;
+
+    Logger(Logger&& src) = delete;
+
     Logger& operator=(const Logger& src) = delete;
     
-    // There are no needs for this operator
     Logger& operator=(Logger&& src) = delete;
 
 protected:
 
 private:
     // не забыть про выравнивание!
+    std::shared_ptr<std::ofstream> logger_file_;
     const std::string filename_;
     LoggerLevel default_level_;
 };
 
 int main(int argc, char *argv[]) {
 
-    std::string test2= "hello world";
-
-    Logger test(test2, LoggerLevel::INFO);
 
     return 0;
 }
