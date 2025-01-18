10da6be7874f00108cf8477974fd7c11fba969c7
Ivan010403 <vany.kozlov5@mail.ru>
2025-01-18 21:02:46 +0500

    test concurency

--- src/main.cpp
+++ src/main.cpp
@@ -7,50 +7,78 @@
 #include <iostream>
 #include <chrono>
 
-struct ThreadData {
-    bool done = false;
-    bool notified = false;
-    
+struct ThreadData {    
     LoggerLevel log_level;
     std::string message;
 };
 
-void sendLogs(const Logger& logger, ThreadData& data, std::condition_variable& cv, std::mutex& mtx) noexcept {
-    while (!data.done) {
+struct Flags {
+    bool done = false;
+    bool logger_notified = false;
+    bool main_notified = true;
+};
+
+void sendLogs(Logger& logger, const ThreadData& data, Flags& flags, std::condition_variable& cv, std::mutex& mtx) noexcept {
+    while (!flags.done) {
+        std::cout << "thread ----> 1\n";
         std::unique_lock<std::mutex> locker(mtx);
-        data.notified = false;
+        std::cout << "thread ----> 2\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
 
 
-        while(!data.notified) // от ложных пробуждений
+        while(!flags.logger_notified) {
+            std::cout << "thread ----> 3\n";
             cv.wait(locker);
+        }
+
+        std::cout << "thread ----> 4\n";
+
+        logger.logInfo(data.message);
+
+        flags.logger_notified = false;
+        flags.main_notified = true;
+        cv.notify_one();
+
+        std::cout << "thread ----> 5\n";
     }
 }
 
 int main(int argc, char *argv[]) {
     Logger test("test.txt", LoggerLevel::INFO);
+    test.logInfo("test!");
     
     ThreadData data;
+    Flags flags;
 
     std::condition_variable cv;
     std::mutex mtx;
 
 
-    std::thread logger_thread(sendLogs, std::cref(test), std::ref(data), std::ref(cv), std::ref(mtx));
+    std::thread logger_thread(sendLogs, std::ref(test), std::cref(data), std::ref(flags), std::ref(cv), std::ref(mtx));
 
 
-    while (true) {
+    while (true) { 
+        std::cout <<"main ---------> 1\n";
+        std::unique_lock<std::mutex> locker(mtx);
+        std::cout <<"main ---------> 2\n";
+
+        while (!flags.main_notified) {
+            std::cout <<"main ---------> 3\n";
+            cv.wait(locker);
+        }
+
         std::cout << "Введите уровень сообщения и его само:\n";
         std::cin >> data.message;
-        std::unique_lock<std::mutex> locker(mtx);
         
-        data.notified = true;
+        flags.logger_notified = true;
+        flags.main_notified = false;
+        std::cout <<"main ---------> 4\n";
+
         cv.notify_one();
+        
     }
-    // test.logInfo("info log");
-    // test.logWarn("warn log");
-    // test.logError("error!");
 
 
-    data.done = true;
+    flags.done = true;
     return 0;
 }
